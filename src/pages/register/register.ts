77817390e0d143255082a883b09ed7d67d4082ab
nativacu <nativacu@gmail.com>
2019-09-09 16:21:52 -0400

    Added account type to account information

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -11,6 +11,7 @@ import { MapPage } from '../map/map';
  * Ionic pages and navigation.
  */
 
+
 @IonicPage()
 @Component({
   selector: 'page-register',
@@ -21,9 +22,12 @@ export class RegisterPage {
   signupEmail:string;
   signupPassword:string;
   fname:string;
+  sname: string;
   lname:string;
+  slname: string;
   id:string;
   phone: string;
+  accountType: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public fauth: AuthProvider, public http: HttpRequestProvider) {
   }
@@ -35,8 +39,12 @@ export class RegisterPage {
   signup(){
     this.fauth.doRegister({"email": this.signupEmail, "password":this.signupPassword}).then(
       (user:firebase.User)=>{
-        console.log('Registered user');
-        this.http.sendPostRequest({cedula: this.id, primernombre: this.fname, segundonombre: 0, primerapellido: this.lname, segundoapellido: 0, t_usuario: 2,
+
+        if(this.slname === null){
+          this.slname = '0';
+        }
+
+        this.http.sendPostRequest({cedula: this.id, primernombre: this.fname, segundonombre: this.sname, primerapellido: this.lname, segundoapellido: this.slname, t_usuario: this.accountType,
           foto: 0, email: this.signupEmail, telefono: this.phone}, 'post.php').then((data) =>{
             this.fauth.currUser.next(data);
             this.navCtrl.setRoot(MapPage);
